9523ae7766cf4fad7dbb5cd0acb8f816b9e53b1d
Badi1298 <serbandavid83@yahoo.com>
2024-12-08 22:22:36 +0200

    core components & styling basics

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on me!</Text>
-            <StatusBar style="auto" />
+            <Text style={{ color: 'green', padding: 16 }}>Aaaaaaaaaaa</Text>
+            <Button title="This is a button" />
+            <View style={styles.innerContainer}>
+                <Text style={{ color: 'green' }}>Aaaaaaaaaaa</Text>
+                <Text style={{ color: 'red' }}>Aaaaaaaaaaa</Text>
+                <Text style={{ color: 'blue' }}>Aaaaaaaaaaa</Text>
+            </View>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
+        display: 'flex',
         flex: 1,
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    innerContainer: {
+        display: 'flex',
+        flexDirection: 'row',
+        columnGap: 16,
+        marginTop: 20,
     },
 });
